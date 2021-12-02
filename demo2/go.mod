aaaf97296044b0a14efd989398476e9d275f9f95
nyuta01 <nyuta2048@gmail.com>
2021-12-02 14:47:34 +0900

    feat: add demo1 & demo2

---
+++ demo2/go.mod
@@ -0,0 +1,3 @@
+module nyuta01/k8s-hot-reload
+
+go 1.17
---
+++ demo2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	for {
+		fmt.Println("demo2 - Hello world!!")
+
+		time.Sleep(time.Second * 10)
+	}
+}
